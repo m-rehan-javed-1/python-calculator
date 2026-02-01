fb53baa83116b8d6b6146ebfb24d05404944aaf9
m-rehan-javed-1 <rehanrao1245@gmail.com>
2026-02-01 23:08:08 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,34 @@
+print("Welcome to Python Calculator")
+def calculator():
+    print()
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Choose operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print()
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == "1":
+        result = num1 + num2
+    elif choice == "2":
+        result = num1 - num2
+    elif choice == "3":
+        result = num1 * num2
+    elif choice == "4":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: Cannot divide by zero "
+    else:
+        result = "Error: Invalid choice "
+    print()
+    print("Result:", result)
+
+while True:
+    calculator()
+    repeat = input("Do you want to continue? (y/n): ")
+    if repeat != "y":
+        break
